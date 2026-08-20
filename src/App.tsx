c0cc56aee04a49e195e7fff72b9f8e9846e1c17b
Khalil Al Jamil <kjamil2004@gmail.com>
2026-08-20 16:47:21 +0300

    Revert "coming soon page"
    This reverts commit 77cabf243af919e701400275c16c4d6fcb267fe1.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,28 @@
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Navigation } from "./components/navigation";
 import { Footer } from "./components/footer";
+import Home from "./pages/home";
 import Portfolio from "./pages/portfolio";
 import { ContactPage } from "./pages/contact";
 import { AboutPage } from "./pages/about";
 import { AlMokhtar } from "./pages/al-mokhtar";
 import { NotFoundPage } from "./pages/not-found";
-import ComingSoon from "./pages/coming-soon";
-
-// Wraps inner routes so we can hide chrome on the coming-soon root
-const AppShell = () => {
-  const location = useLocation();
-  const isComingSoon = location.pathname === "/";
-
-  return (
-    <div className="min-h-screen bg-white">
-      {!isComingSoon && <Navigation />}
-      <Routes>
-        <Route path="/" element={<ComingSoon />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/projects/al-mokhtar" element={<AlMokhtar />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-      {!isComingSoon && <Footer />}
-    </div>
-  );
-};
 
 const App = () => {
   return (
     <Router>
-      <AppShell />
+      <div className="min-h-screen bg-white">
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/projects/al-mokhtar" element={<AlMokhtar />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 };
